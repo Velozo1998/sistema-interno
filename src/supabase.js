d002c506ecdbaaa0ca21994781ad55498e6f2047
Velozo1998 <velozo.hugo@gmail.com>
2026-05-20 19:36:35 +0000

    fix: trocar sb_publishable por anon key padrão

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mjttwalhvseuxeoyivre.supabase.co'
-const supabaseKey = 'sb_publishable_-ZW4htliwBkZHcf_HX4dLA_iZgDqgQH'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qdHR3YWxodnNldXhlb3lpdnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NjI4ODAsImV4cCI6MjA5MjQzODg4MH0.baDd0AScHBqF_5v36D7w-rQIDxj-37Z69qndGW_at3w'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
